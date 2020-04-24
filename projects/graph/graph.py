27022385ab02dcfd2bee7fb6926f92a36e7d66e3
Greyflanel <richardlovelace811@gmail.com>
2020-04-24 16:28:43 -0500

    all but recursions are complete for graph.py. Also added word_ladder1.py from the lecture.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,8 +1,8 @@
 """
 Simple graph implementation
 """
-from util import Stack, Queue  # These may come in handy
 
+from util import Stack, Queue  # These may come in handy
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -96,25 +96,32 @@ class Graph:
         breath-first order.
         """
         # create a empty queue, and enqueue a PATH to the starting vertex
-        # queue.enqueue([starting_vertex])
-        queue = Queue()
-        queue.enqueue([starting_vertex])
+        neighbors_to_visit = Queue()
+        neighbors_to_visit.enqueue([starting_vertex])
         # create a set for visited vertices
         visited_vertices = set()
         # while the queue is not empty
-            # dequeue the first PATH
-        while queue.size() > 0:
-            current_vertex = queue.dequeue()
+        while neighbors_to_visit.size() > 0:
+            # dequeue the first PATH in the queue 
+            current_path = neighbors_to_visit.dequeue() 
             # grab the last vertex in the path
+            current_vertex = current_path[-1]
             # if it hasn't been visited 
+            if current_vertex not in visited_vertices:
                 # check if its the target 
+                if current_vertex == destination_vertex:
+                    return current_path
                     # Return the path 
                 # mark it as visited
+                visited_vertices.add(current_vertex)
                 # make new versions of the current path, with each neighbor added to them
+                for next_vertex in self.get_neighbors(current_vertex):
                     # duplicate the path
+                    new_path = list(current_path)
                     # add the neighbor
+                    new_path.append(next_vertex)
                     # add the new path to the queue
-
+                    neighbors_to_visit.enqueue(new_path)
 
         pass  # TODO
 
@@ -124,7 +131,37 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        # This solution takes a slightly different approach as to how we are storing the path
+        # Now, we always queue up the next vertex we want to see, and a list of all the vertices we looked at to get here
+        # so if we are queueing up vertex 3 from our example, the tuple we create will be (3, [1,2])
+        # because we had to go through 1 and 2 to get here
+        neighbors_to_visit = Stack()
+        visited_vertices = set()
+        # add the first vertex, and an empty list indicating that we have not been to any other vertices yet
+        neighbors_to_visit.push((starting_vertex, []))
+        # loop through the stack
+        while neighbors_to_visit.size() > 0:
+            # This will have (current_vertex, path)
+            current_vertex_plus_path = neighbors_to_visit.pop()
+            # pull out the current vertex so its easier to read
+            current_vertex = current_vertex_plus_path[0]
+            # pull out the path so its easier to read
+            current_path = current_vertex_plus_path[1]
+            # make sure the vertex isnt something we have seen already
+            if current_vertex not in visited_vertices:
+
+                # if the vertex is the destination return it plus the path we took to get here
+                if current_vertex == destination_vertex:
+                    # eg: if the vertex was 6, and we went through 1, 2, 4 to get here, add that to complete the full path
+                    updated_path = current_path + [current_vertex]
+                    return updated_path
+
+                # mark the vertex as visited
+                visited_vertices.add(current_vertex)
+                # add neighbors to the stack
+                for neighbor in self.get_neighbors(current_vertex):
+                    updated_path = current_path + [current_vertex]
+                    neighbors_to_visit.push((neighbor, updated_path))
 
     def dfs_recursive(self, starting_vertex, destination_vertex):
         """
@@ -196,7 +233,8 @@ if __name__ == '__main__':
         [1, 2, 4, 6]
     '''
     print(graph.bfs(1, 6))
-
+    print("DFS")
+    
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
@@ -204,4 +242,3 @@ if __name__ == '__main__':
     '''
     print(graph.dfs(1, 6))
     print(graph.dfs_recursive(1, 6))
-
